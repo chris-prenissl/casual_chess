6ed18debfc76cb349f0ad89fd67c3629eed8318d
snakept <snakept@icloud.com>
2021-01-10 19:25:49 +0100

    Initial Commit

---
+++ Casual Chess/Casual_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Casual_ChessApp.swift
+//  Casual Chess
+//
+//  Created by Christoph Prenissl on 10.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Casual_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Casual Chess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Casual Chess
+//
+//  Created by Christoph Prenissl on 10.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
