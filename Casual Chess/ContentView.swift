bf991b505ab8ff2ae47c1a35501314a68cb8dade
snakept <snakept@icloud.com>
2021-01-11 17:41:49 +0100

    updated assets so pieces aren't translucent

--- Casual Chess/ContentView.swift
+++ Casual Chess/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("chess_board")
+        ZStack {
+            Image("chess_board")
+            Image("bishop_b")
+                
+        }
     }
 }
 
