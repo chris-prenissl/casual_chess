37511724c740af3468f8a731c29bac61ffcb3144
snakept <snakept@icloud.com>
2021-01-13 23:32:14 +0100

    Chess UI now looks nice

--- Casual Chess/ContentView.swift
+++ Casual Chess/ContentView.swift
@@ -7,31 +7,65 @@
 
 import SwiftUI
 
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+}
+
 struct ContentView: View {
     
     let board = ChessBoard()
+    let sizeFactor: CGFloat = 8.75
     
     var body: some View {
         ZStack {
-            Image("chess_board")
-                .resizable()
-                .frame(width: CGFloat(board.rows) * 100, height: CGFloat(board.columns) * 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            ChessGrid(rows: 8, columns: 8, board: ChessBoard())
+            LinearGradient(gradient: Gradient(colors: [Color.black, Color("darkBrown"), Color("lightBrown")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            ChessBoardGrid(sizeFactor: sizeFactor)
+            ChessPiecesGrid(rows: 8, columns: 8, board: ChessBoard(), sizeFactor: sizeFactor)
         }
     }
 }
 
-struct ChessGrid: View {
+struct ChessBoardGrid: View {
+    let sizeFactor: CGFloat
+    
+    var body: some View {
+        ZStack {
+            Color("darkBrown")
+                .frame(width: UIScreen.screenWidth * 0.96, height: UIScreen.screenWidth * 0.96, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0){
+                ForEach(0..<8, id: \.self) { row in
+                    HStack(alignment: .center, spacing: 0) {
+                        ForEach(0..<8, id: \.self) { column in
+                            if ((row % 2 == 0 && column % 2 == 0) || (row % 2 == 1 && column % 2 == 1)) {
+                                Color("lightBrown")
+                                    .frame(width: UIScreen.screenWidth / sizeFactor, height: UIScreen.screenWidth / sizeFactor, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            } else {
+                                Color("darkBrown")
+                                    .frame(width: UIScreen.screenWidth / sizeFactor, height: UIScreen.screenWidth / sizeFactor, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ChessPiecesGrid: View {
     let rows: Int
     let columns: Int
     let board: ChessBoard
+    let sizeFactor: CGFloat
     
     var body: some View {
-        VStack {
+        VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0){
             ForEach(0..<rows, id: \.self) { row in
-                HStack {
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0){
                     ForEach(0..<columns, id: \.self) { column in
-                        PieceView(piece: board.board[row][column])
+                        PieceView(piece: board.board[row][column], sizeFactor: sizeFactor)
                     }
                 }
             }
@@ -41,13 +75,17 @@ struct ChessGrid: View {
 
 struct PieceView : View {
     let piece: Piece?
+    let sizeFactor : CGFloat
     
     var body: some View {
         if (piece != nil) {
             Image(piece!.url)
+                .resizable()
+                .frame(width: UIScreen.screenWidth / sizeFactor, height: UIScreen.screenWidth / sizeFactor, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         } else {
             Image("")
-                .frame(width: 90, height: 90, alignment: .center)
+                .resizable()
+                .frame(width: UIScreen.screenWidth / sizeFactor, height: UIScreen.screenWidth / sizeFactor, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
         
     }
